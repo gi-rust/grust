a3dfea2e1d74f1158996609ba2a13b0783f1125a
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2014-09-30 01:02:10 +0300

    Removed the Unowned type

--- grust/refcount.rs
+++ grust/refcount.rs
@@ -62,33 +62,17 @@ pub mod raw {
 
 }
 
-pub struct Unowned<'a, T: 'a + Refcount> {
-    ptr: *mut T,
-    lifetime_marker: marker::ContravariantLifetime<'a>
-}
-
 unsafe fn make_ref<T: Refcount>(p: *mut T) -> Ref<T> {
     (*p).inc_ref();
     raw::ref_from_ptr(p)
 }
 
-unsafe fn make_unowned<'a, T: 'a + Refcount>(p: *mut T) -> Unowned<'a, T> {
-    Unowned {
-        ptr: p,
-        lifetime_marker: marker::ContravariantLifetime
-    }
-}
-
 impl<T: Refcount> Ref<T> {
-    pub fn new(source: &Unowned<T>) -> Ref<T> {
-        unsafe { make_ref(source.ptr) }
+    pub fn new(source: &mut T) -> Ref<T> {
+        unsafe { make_ref(source as *mut T) }
     }
     pub fn borrow<'a>(&'a self) -> &'a T { unsafe { &*self.ptr } }
     pub fn borrow_mut<'a>(&'a mut self) -> &'a mut T { unsafe { &mut *self.ptr } }
-}
-
-impl<'a, T: 'a + Refcount> Unowned<'a, T> {
-    pub fn new_ref(&self) -> Ref<T> { unsafe { make_ref(self.ptr) } }
 }
 
 #[unsafe_destructor]
@@ -118,13 +102,5 @@ impl<T: Refcount> Deref<T> for Ref<T> {
 }
 
 impl<T: Refcount> DerefMut<T> for Ref<T> {
-    fn deref_mut<'a>(&'a mut self) -> &'a mut T { unsafe { &mut *self.ptr } }
-}
-
-impl<'b, T: Refcount> Deref<T> for Unowned<'b, T> {
-    fn deref<'a>(&'a self) -> &'a T { unsafe { &*self.ptr } }
-}
-
-impl<'b, T: Refcount> DerefMut<T> for Unowned<'b, T> {
     fn deref_mut<'a>(&'a mut self) -> &'a mut T { unsafe { &mut *self.ptr } }
 }
