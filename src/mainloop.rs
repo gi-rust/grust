f3182627bc5c701ccd7eccc7397c106dacac7d37
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2015-07-02 04:17:09 +0300

    mainloop: Add infrastructure for source callbacks
    Callback types are constructed from closures typed appropriately for
    the callback signature. All callbacks should provide a transformation
    into the RawCallback structure which is used to provide the callback data
    for the C APIs.

--- src/mainloop.rs
+++ src/mainloop.rs
@@ -1,6 +1,6 @@
 // This file is part of Grust, GObject introspection bindings for Rust
 //
-// Copyright (C) 2014  Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
+// Copyright (C) 2014, 2015  Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
@@ -17,12 +17,103 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 use refcount::{Refcount, Ref};
-use types::FALSE;
+use types::{FALSE, TRUE};
+use types::{gboolean, gint, gpointer, guint};
+use util::{box_free, box_from_pointer, box_into_pointer, into_destroy_notify};
 use wrap;
 use wrap::Wrapper;
 
 use glib as ffi;
 use gobject;
+use std::convert;
+use std::marker;
+use std::mem;
+
+pub const PRIORITY_DEFAULT      : gint = ffi::G_PRIORITY_DEFAULT;
+pub const PRIORITY_DEFAULT_IDLE : gint = ffi::G_PRIORITY_DEFAULT_IDLE;
+pub const PRIORITY_HIGH         : gint = ffi::G_PRIORITY_HIGH;
+pub const PRIORITY_HIGH_IDLE    : gint = ffi::G_PRIORITY_HIGH_IDLE;
+pub const PRIORITY_LOW          : gint = ffi::G_PRIORITY_LOW;
+
+pub enum CallbackResult { Remove, Continue }
+pub use self::CallbackResult::*;
+
+pub struct RawCallback {
+    func: ffi::GSourceFunc,
+    data: gpointer,
+    destroy: ffi::GDestroyNotify
+}
+
+impl Drop for RawCallback {
+    fn drop(&mut self) {
+        (self.destroy)(self.data);
+    }
+}
+
+unsafe fn into_source_func(func: unsafe extern "C" fn(gpointer) -> gboolean)
+                          -> ffi::GSourceFunc
+{
+    mem::transmute(func)
+}
+
+unsafe extern "C" fn source_func<F>(callback_data: gpointer) -> gboolean
+    where F: FnMut() -> CallbackResult
+{
+    let mut callback: Box<F> = box_from_pointer(callback_data);
+    let res = callback();
+    mem::forget(callback);
+    match res {
+        Remove => FALSE,
+        Continue => TRUE
+    }
+}
+
+unsafe extern "C" fn source_once_func<F>(callback_data: gpointer) -> gboolean
+    where F: FnOnce()
+{
+    let mut holder: Box<Option<F>> = box_from_pointer(callback_data);
+    let callback = holder.take().expect("a callback closure expected");
+    mem::forget(holder);
+    callback();
+    FALSE
+}
+
+pub struct SourceCallback(RawCallback);
+
+impl Into<RawCallback> for SourceCallback {
+    #[inline]
+    fn into(self) -> RawCallback {
+        self.0
+    }
+}
+
+impl SourceCallback {
+    pub fn new<F>(closure: F) -> Self
+        where F: Send + 'static, F: FnMut() -> CallbackResult
+    {
+        let boxed_closure = Box::new(closure);
+        SourceCallback(unsafe {
+            RawCallback {
+                func: into_source_func(source_func::<F>),
+                data: box_into_pointer(boxed_closure),
+                destroy: into_destroy_notify(box_free::<F>)
+            }
+        })
+    }
+
+    pub fn once<F>(closure: F) -> Self
+        where F: Send + 'static, F: FnOnce()
+    {
+        let holder = Box::new(Some(closure));
+        SourceCallback(unsafe {
+            RawCallback {
+                func: into_source_func(source_once_func::<F>),
+                data: box_into_pointer(holder),
+                destroy: into_destroy_notify(box_free::<Option<F>>)
+            }
+        })
+    }
+}
 
 #[repr(C)]
 pub struct MainContext {
